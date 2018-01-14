873d88dc473742f5253b4d4a6105cd62b34c9d7a
moovlin <richisyoursavior@gmail.com>
2018-01-14 01:55:06 -0500

    Created a Videos class as a way to access data and be sure data exists. The plan for this object is to be able to query and update a sqllite data base to help     access times and also have rention between runs. Also added a remove     function to be able to remove a video by it's URL.

--- youtube_dl_list.py
+++ youtube_dl_list.py
@@ -4,26 +4,63 @@ import threading
 
 app = Flask(__name__)
 
-videos = []
+
+class Videos:
+
+    def __init__(self):
+        self.videos = []
+
+
+    def add(self, video):
+        self.videos.append(video)
+
+
+    def remove(self, video):
+        self.videos.remove(video)
+
+
+    def get_video(self):
+        return self.videos.pop()
+
+
+    def while_video(self):
+        print("videos: " + str(self.videos))
+        if self.videos != []:
+            return True
+        return False
+
+    def get_list(self):
+        for index in range(len(self.videos)-1, -1, -1):
+            yield self.videos[index]
+
+
+videos = Videos()
 
 @app.route('/')
 def list():
-    return str(videos)
+    output = ""
+    for vid in videos.get_list():
+        output += "\n" + vid
+    return output
+
 
 @app.route('/add', methods=['POST','GET'])
 def  download_video():
     vid = request.args.get('v', type = str)
-    if vid is not None:
-        videos.append(str(vid))
+    if vid is not None and vid != '':
+        videos.add(str(vid))
 
     return "Success"
+
 
 @app.route('/download')
 def download():
     ydl_opts = {}
     errors = []
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        for vid in videos:
+        while videos.while_video():
+            vid = videos.get_video()
+            print("vid: " + vid)
             try:
                 ydl.download([vid])
                 videos.remove(vid)
@@ -34,4 +71,18 @@ def download():
         return "DONE"
     else:
         return str(errors)
+
+
+@app.route('/remove', methods=['GET'])
+def remove():
+    vid = request.args.get('v', type = str)
+    if vid is not None and vid != '':
+        try:
+            videos.remove(str(vid))
+        except:
+            return "Could not remove " + str(vid)
+
+    return "Success"
+
+
 

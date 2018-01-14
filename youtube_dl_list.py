17a160dc04077cda718e7f7218d558cd93ae25b7
moovlin <richisyoursavior@gmail.com>
2018-01-14 01:22:27 -0500

    Download now reports which video URLS caused an error and only returns those errors if an error occurs.

--- youtube_dl_list.py
+++ youtube_dl_list.py
@@ -1,5 +1,6 @@
 from flask import Flask,request
 import youtube_dl
+import threading
 
 app = Flask(__name__)
 
@@ -13,20 +14,24 @@ def list():
 def  download_video():
     vid = request.args.get('v', type = str)
     if vid is not None:
-        videos.append(vid)
+        videos.append(str(vid))
 
     return "Success"
 
 @app.route('/download')
 def download():
     ydl_opts = {}
+    errors = []
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         for vid in videos:
             try:
                 ydl.download([vid])
                 videos.remove(vid)
             except:
-                return "ERROR: " + str(vid)
+                errors.append("ERROR: " + str(vid))
 
-    return "DONE"
+    if errors == []:
+        return "DONE"
+    else:
+        return str(errors)
 
